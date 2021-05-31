f583676e94cba0cf022ed3cb523f1b904949d5df
Amirmahdi Tafreshi <a.tafreshi440@gmail.com>
2021-06-01 02:00:49 +0430

    Correct typos. "Mastadon" to "Mastodon"

--- app/BotUpdater.py
+++ app/BotUpdater.py
@@ -8,10 +8,10 @@ import json
 
 myclient = pymongo.MongoClient("mongodb://"+os.environ['MONOG_DATABASE_USERNAME']+":"+os.environ['MONOG_DATABASE_PASSWORD']+"@"+os.environ['MONOG_DATABASE_ADDRESS'])
 DB = myclient[os.environ['MONGO_DATABASE_NAME']]
-mastadon_connections_collation = DB["mastadon_connections"]
+mastodon_connections_collation = DB["mastodon_connections"]
 
-def get_last_mastadon_notification_id(mastadon_server_address, access_token) -> dict:
-    endpoint_url = mastadon_server_address + os.environ['MASTADON_API_NOTIFICATION_ADDRESS']
+def get_last_mastodon_notification_id(mastodon_server_address, access_token) -> dict:
+    endpoint_url = mastodon_server_address + os.environ['MASToDON_API_NOTIFICATION_ADDRESS']
     headers = {"Authorization": "Bearer " + access_token}
     response = requests.get(endpoint_url, headers=headers)
     notifications = json.loads(response.text)
@@ -21,9 +21,9 @@ def get_last_mastadon_notification_id(mastadon_server_address, access_token) -> 
 def start(update: Update, context: CallbackContext) -> None:
     chat_id = update.message.chat_id
     first_name = update.message.from_user.first_name
-    update.message.bot.send_message(chat_id=chat_id, text=f"Hello {first_name}. Welcome to the Mastadon notifier bot!")
+    update.message.bot.send_message(chat_id=chat_id, text=f"Hello {first_name}. Welcome to the Mastodon notifier bot!")
 
-def add_mastadon_account_help(update: Update, context: CallbackContext) -> None:
+def add_mastodon_account_help(update: Update, context: CallbackContext) -> None:
         text = "To add your Mastodon account, you must send your server address and password (with read notification) as the next message."
         update.message.bot.send_message(chat_id=update.message.chat_id, text=text)
         message_example = """https://mas.to
@@ -33,12 +33,12 @@ def add_mastadon_account_help(update: Update, context: CallbackContext) -> None:
 
 
 
-def add_mastadon_account(update: Update, context: CallbackContext) -> None:
-    mastadon_server_address, access_token= update.message.text.splitlines()
-    last_notification_id = get_last_mastadon_notification_id(mastadon_server_address, access_token)
-    mastadon_connections_collation.insert_one({"first_name": update.message.from_user.first_name, 
+def add_mastodon_account(update: Update, context: CallbackContext) -> None:
+    mastodon_server_address, access_token= update.message.text.splitlines()
+    last_notification_id = get_last_mastodon_notification_id(mastodon_server_address, access_token)
+    mastodon_connections_collation.insert_one({"first_name": update.message.from_user.first_name, 
                                                "chat_id": update.message.chat_id,
-                                               "mastadon_server_address": mastadon_server_address, 
+                                               "mastodon_server_address": mastodon_server_address, 
                                                "access_token": access_token,
                                                "last_notification_id": last_notification_id,})
     update.message.bot.send_message(chat_id=update.message.chat_id, text="Your account has been successfully added.")
@@ -53,8 +53,8 @@ def main():
     updater = Updater(os.environ['TELEGRAM_BOT_TOKEN'])
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("add_mastadon_account", add_mastadon_account_help))
-    dispatcher.add_handler(MessageHandler(Filters.text, add_mastadon_account))
+    dispatcher.add_handler(CommandHandler("add_mastodon_account", add_mastodon_account_help))
+    dispatcher.add_handler(MessageHandler(Filters.text, add_mastodon_account))
     updater.start_polling()
     updater.idle()
 
